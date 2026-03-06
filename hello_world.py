cd4b351a1335ff85013989f5e75881f1407ea0bb
Armand Baril <amb35@sfu.ca>
2026-03-06 14:20:43 -0800

    initial commit

--- hello_world.py
+++ hello_world.py
@@ -2,7 +2,7 @@ import mitsuba as mi
 
 mi.set_variant('scalar_rgb')
 
-img = mi.render(mi.load_dict(mi.cornell_box()))
-ldr = mi.Bitmap(img).convert(mi.Bitmap.PixelFormat.RGB, mi.Struct.Type.UInt8)
+img = mi.render(mi.load_file('point_grid_scene.xml'))
+ldr = mi.Bitmap(img).convert(mi.Bitmap.PixelFormat.RGB, mi.Struct.Type.UInt8, srgb_gamma=True)
 
-ldr.write('cbox.png')
+ldr.write('result.png')
